0b82578ee628fca0382889e0d9d94669c1369384
Your Name <you@example.com>
2023-03-16 12:32:55 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /* main this is printin the function */
- /*
-  * main - A program that print a line using printf function
-  *Return: 0 (Success)
-  *
+/**
+  * main - short descriptin, single line
+ (* a blank line
+  * Description: Longer description of the function)?
+ (*section header: Section description)*
+  *Return: Description of the returned value void string
+  *main - prints "with proper grammer, but the outcome is a piece of art, \n"
   */
 int main(void)
 {	printf("with proper grammar, but the outcome is a piece of art,\n");
