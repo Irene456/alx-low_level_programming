82bcd12a418d21c81d0a980775e19e97e1b7130c
Your Name <you@example.com>
2023-03-16 14:11:38 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  * Return: Description of the returned value
  */
 int main(void)
-{	printf("Size of a char: %li byte(s)/n", sizeof(char));
-	printf("Size of int: %li byte(s)/n", sizeof(int));
-	printf("Size of a long: %li byte(s)/n", sizeof(long int));
-	printf("Size of a long long int: %li byte(s)/n", sizeof(long long int));
-	printf("Size of a float: %li byte(s)/n", sizeof(float));
+{	printf("Size of a char: %li byte(s)\n", sizeof(char));
+	printf("Size of int: %li byte(s)\n", sizeof(int));
+	printf("Size of a long: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
