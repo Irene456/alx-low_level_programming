a3a0f16ce5b59fd4096ceb651881953b0a9b0203
Your Name <you@example.com>
2023-03-17 09:19:08 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints alpha in lowercase
+ * Return: always 0
+ */
+int main(void)
+{	char c;
+	c= 'a';
+	while 
+	{
+		putchar(c);
+		c++;
+	}
+	putchar ("\n#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
